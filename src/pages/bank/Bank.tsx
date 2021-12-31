b18b40795f291227e720d95728ff0cf5b342d100
kazufoot21 <kazufoot21@outlook.jp>
2021-12-31 16:44:22 +0900

    stuling

--- src/pages/bank/Bank.tsx
+++ src/pages/bank/Bank.tsx
@@ -3,6 +3,7 @@ import styles from './Bank.module.scss';
 
 type State = {
   savings: number;
+  histories: { operation: string; amout: number }[];
 };
 
 type Action =
@@ -11,14 +12,29 @@ type Action =
 
 const initialState = {
   savings: 100000,
+  histories: [],
 };
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case 'DEPOSIT':
-      return { ...state, savings: state.savings + action.payload };
+      return {
+        ...state,
+        savings: state.savings + action.payload,
+        histories: [
+          ...state.histories,
+          { operation: '預け入れ', amout: action.payload },
+        ],
+      };
     case 'WITHDRAW':
-      return { ...state, savings: state.savings - action.payload };
+      return {
+        ...state,
+        savings: state.savings - action.payload,
+        histories: [
+          ...state.histories,
+          { operation: '引き出し', amout: action.payload },
+        ],
+      };
     default:
       return state;
   }
@@ -30,9 +46,11 @@ export const Bank: React.VFC = () => {
 
   const onDeposit = (amout: number) => {
     dispatch({ type: 'DEPOSIT', payload: amout });
+    setAmout(0);
   };
   const onWithdraw = (amout: number) => {
     dispatch({ type: 'WITHDRAW', payload: amout });
+    setAmout(0);
   };
 
   return (
@@ -51,6 +69,28 @@ export const Bank: React.VFC = () => {
             <button onClick={() => onWithdraw(amout)}>引き出し</button>
           </div>
         </div>
+      </div>
+      <div className={styles.history}>
+        <h4>履歴</h4>
+        {state.histories.map((history, index) => {
+          return history.operation === '引き出し' ? (
+            <p key={index} className={styles.withdraw}>
+              {history.operation} : {history.amout.toLocaleString()}円
+            </p>
+          ) : (
+            <p key={index} className={styles.deposit}>
+              {history.operation} : {history.amout.toLocaleString()}円
+            </p>
+          );
+
+          // --------------------------------------
+          // return (
+          //   <p key={index}>
+
+          //     {history.operation} : {history.amout.toLocaleString()}円
+          //   </p>
+          // );
+        })}
       </div>
     </div>
   );
